135d4cd18c912aa5bcd754c0bffcdb9a63e78154
Àngel <angel@rom.cat>
2022-09-22 22:28:34 +0200

    Agregar archivos de proyecto.

---
+++ 01_Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace _01_Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+            //// Web API configuration and services
+            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            //GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            //// New code
+            //config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ 01_Api/Controllers/EmpleadosController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using _04_Data.Data;
+
+namespace _01_Api.Controllers
+{
+    //[EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+
+    public class EmpleadosController : ApiController
+    {
+        private NorthWindTuneadoDbContext db = new NorthWindTuneadoDbContext();
+
+        // GET: api/Empleados
+        public IList<Empleado> GetEmpleado()
+        {
+            var empleados = db.Empleado;
+            IList<Empleado> empleadosSinHijos = new List<Empleado>();
+            foreach (Empleado empleado in empleados)
+            {
+                Empleado empleadoSinHijos = new Empleado();
+                empleadoSinHijos.EmployeeID = empleado.EmployeeID;
+                empleadoSinHijos.FirstName = empleado.FirstName;
+                empleadoSinHijos.LastName  = empleado.LastName ;
+                empleadosSinHijos.Add(empleadoSinHijos);
+            }
+            return empleadosSinHijos;
+        }
+
+        // GET: api/Empleados/5
+        [ResponseType(typeof(Empleado))]
+        public IHttpActionResult GetEmpleado(int id)
+        {
+            Empleado empleado = db.Empleado.Find(id);
+
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            Empleado empleadoSinHijos = new Empleado();
+            empleadoSinHijos.EmployeeID = empleado.EmployeeID;
+            empleadoSinHijos.FirstName = empleado.FirstName;
+            empleadoSinHijos.LastName = empleado.LastName;
+
+            return Ok(empleadoSinHijos);
+        }
+
+        // PUT: api/Empleados/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmpleado(int id, Empleado empleado)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != empleado.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Empleados
+        [ResponseType(typeof(Empleado))]
+        public IHttpActionResult PostEmpleado(Empleado empleado)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Empleado.Add(empleado);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = empleado.EmployeeID }, empleado);
+        }
+
+        // DELETE: api/Empleados/5
+        [ResponseType(typeof(Empleado))]
+        public IHttpActionResult DeleteEmpleado(int id)
+        {
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            db.Empleado.Remove(empleado);
+            db.SaveChanges();
+
+            return Ok(empleado);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return db.Empleado.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
